ee47abd57027cdfd9d66090045f28d86e6e2c07a
Yurii Ts <yura9989@gmail.com>
2025-02-14 11:25:22 +0200

    add orders entity

---
+++ src/orders/entity/orders.entity.ts
@@ -0,0 +1,34 @@
+// src/orders/order.entity.ts
+import { User } from 'src/users/entity/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+export enum OrderStatus {
+  NEW = 'new',
+  PROCESSING = 'processing',
+  SHIPPED = 'shipped',
+  DELIVERED = 'delivered',
+}
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, user => user.id)
+  user: User;
+
+  @Column('json')
+  items: string[];
+
+  @Column('decimal')
+  totalPrice: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.NEW })
+  status: OrderStatus;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
